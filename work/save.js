68b9cd6b6a9f996f70fa38e4538145461831351b
DanoR <zheung@vip.qq.com>
2017-04-21 09:52:13 +0800

    改进日志功能

--- work/save.js
+++ work/save.js
@@ -1,16 +1,15 @@
-let down = async(url, iid, proc, socket, ext, ts) => {
+let down = async(url, iid, proc, ext, ts, sog) => {
 	try {
 		let getStream = await func.get(url[0], 2, false);
 
 		await new Promise((resolve, reject) => {
 			let total, passed = 0,
-				writeStream = fs.createWriteStream(`save/${iid}_p${url[1]}.${ext}`)
+				writeStream = fs.createWriteStream(`./save/${iid}_p${url[1]}.${ext}`)
 				.on('drain', () => {
 					getStream.resume();
 				})
 				.on('finish', () => {
-					rog(socket, ts, '下载', iid, proc, '完成');
-					log('下载', iid, proc, '完成');
+					sog.r(ts, '下载', iid, proc, '完成');
 
 					resolve();
 				});
@@ -29,7 +28,7 @@ let down = async(url, iid, proc, socket, ext, ts) => {
 					if(writeStream.write(chunk) == false)
 						getStream.pause();
 
-					rog(socket, ts, '下载', iid, proc, Math.round(passed * 100 / total)+'%');
+					sog.r(ts, '下载', iid, proc, Math.round(passed * 100 / total)+'%');
 				})
 				.on('end', () => {
 					writeStream.end();
@@ -44,22 +43,22 @@ let down = async(url, iid, proc, socket, ext, ts) => {
 	}
 };
 
-module.exports = async(iid, time, socket) => {
+module.exports = async(iid, time, sog) => {
 	let info = JSON.parse((await func.get(`https://www.pixiv.net/rpc/index.php?mode=get_illust_detail_by_ids&illust_ids=${iid}`, 1)).toString()),
 		urls = [], pid = 0, count = ~~info.body[iid].illust_page_count, ext = info.body[iid].illust_ext;
 
 	while(pid < count)
 		urls.push([`http://i.pximg.net/img-original/img/${time}/${iid}_p${pid}.${ext}`, pid++]);
 
-	sog(socket, '下载', iid, `共${count}张`);
+	sog.ll('下载', iid, `共${count}张`);
 
 	await Promise.map(urls, async(url, now) => {
 		let tid = 0, proc = `(${now+1}/${pid})`, retry = ~~conf.retry, ts = `${new Date().getTime()}${url[1]}`;
 
-		rog(socket, ts, '下载', iid, proc);
+		sog.r(ts, '下载', iid, proc);
 
-		while(!await down(url, iid, proc, socket, ext, ts) && tid++ <= retry)
-			sog(socket, '下载', iid, proc, '失败', `第(${tid}/${retry})次重试`);
+		while(!await down(url, iid, proc, ext, ts, sog) && tid++ <= retry)
+			sog.ll('下载', iid, proc, '失败', `第(${tid}/${retry})次重试`);
 	});
 
 	return pid;
