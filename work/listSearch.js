d39de3168ecbb3ab74e05a879ea0c1003e65c2e4
DanoR <zheung@vip.qq.com>
2017-09-11 22:46:02 +0800

    search fix

--- work/listSearch.js
+++ work/listSearch.js
@@ -4,11 +4,14 @@ module.exports = async (word, page, r18) => {
 			str = buf.toString();
 
 		let take = (err, window) => {
-			let $ = window.$, result = [];
+			str;
+			let $ = window.$, result = [],
+				cat1 = $('.column-search-result li.image-item'),
+				cat2 = $('#js-mount-point-search-result-list').data('items');
 
-			$('.column-search-result li.image-item').each(function() {
+			cat1.each(function() {
 				let $$ = $(this), img = $$.find('img._thumbnail');
-
+				L(img.data('type'));
 				result.push({
 					iid: img.data('id'),
 					title: $$.find('h1.title').attr('title'),
@@ -21,6 +24,21 @@ module.exports = async (word, page, r18) => {
 					ugoira: $$.find('a._work').hasClass('ugoku-illust')? 1 : 0
 				});
 			});
+
+			if(cat2)
+				for(let data of cat2) {
+					result.push({
+						iid: data.illustId,
+						title: data.illustTitle,
+						uid: data.userId,
+						user: data.userName,
+						tags: data.tags,
+						time: data.url.match(/(\d{4}\/)(\d{2}\/){4}(\d{2})/g)[0],
+						type: ['illust', '', 'ugoria'][~~data.illustType],
+						multi: data.pageCount > 1  ? 1 : 0,
+						ugoira: data.illustType == 2 ? 1 : 0
+					});
+				}
 
 			resolve(result);
 		};
