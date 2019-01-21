90dea19ca57a124175ac7e502b6dfb6bc97edc2f
DanoR <zheung@vip.qq.com>
2019-01-21 16:14:50 +0800

    update playUgoira

--- font/playUgoira/index.vue
+++ font/playUgoira/index.vue
@@ -1,6 +1,12 @@
 <template>
 	<div class="picBox" ref="box">
-		<canvas class="picCan" @click="pp" ref= "can" :style="{ left: `calc(50% - ${width/2}px)`, top: `calc(50% - ${height/2}px)` }"></canvas>
+		<canvas ref= "can" tabindex="1" class="picCan" :class="{ mdown: mDown }"
+			@keydown="onKeyDown"
+			@wheel="onWheel"
+			@mousedown="onMouseDown"
+			@mouseup="onMouseUp"
+			@mousemove="onMouseMove"
+		></canvas>
 	</div>
 </template>
 
@@ -14,13 +20,19 @@
 
 				illust: {}
 			}, {
-				width: 0,
-				height: 0,
-
-				current: 0,
+				idxNow: 0,
+				picNow: {},
 
 				timeout: 0,
 				interval: 0,
+
+				offW: 0,
+				offH: 0,
+
+				zoom: 50,
+
+				mDown: false,
+				mMove: false,
 
 				loaded: 0,
 				frames: []
@@ -30,6 +42,9 @@
 		created: function() {
 			A.reg('api/infoUgoira', 'api/infoUgoira');
 		},
+		mounted: function() {
+			this.$refs.can.focus();
+		},
 
 		watch: {
 			'S.illust': async function(now) {
@@ -38,24 +53,131 @@
 		},
 
 		methods: {
-			pp: function() {
+			onPause: function() {
 				if(this.timeout) {
 					clearTimeout(this.timeout);
 
 					this.timeout = 0;
 				}
 				else {
-					this.playFrame(this.current);
+					this.playFrame(this.idxNow);
 				}
 			},
+			onKeyDown: function(e) {
+				if(e.keyCode == 32) {
+					this.onPause();
+				}
+				else if(e.keyCode == 107) {
+					this.zoom = this.zoom + 10;
+				}
+				else if(e.keyCode == 109 && this.zoom - 10 > 0) {
+					this.zoom = this.zoom - 10;
+				}
+				else {
+					let off = 1;
+
+					if(e.ctrlKey) { off = 100; }
+					if(e.altKey) { off = 1; }
+
+					if(e.keyCode == 37) {
+
+						this.offW -= off;
+					}
+					else if(e.keyCode == 38) {
+						this.offH -= off;
+					}
+					else if(e.keyCode == 39) {
+						this.offW += off;
+					}
+					else if(e.keyCode == 40) {
+						this.offH += off;
+					}
+				}
+			},
+			onWheel: function(e) {
+				if(e.deltaY > 0) {
+					this.zoom = this.zoom + 10;
+				}
+				else if(this.zoom - 10 > 0) {
+					this.zoom = this.zoom - 10;
+				}
+
+				this.reloadFrame();
+			},
+			onMouseDown: function() {
+				this.$set(this, 'mDown', true);
+				this.$set(this, 'mMove', false);
+			},
+			onMouseUp: function() {
+				this.$set(this, 'mDown', false);
+
+				if(!this.mMove) {
+					this.onPause();
+				}
+			},
+			onMouseMove: function(e) {
+				if(this.mDown && e.buttons == 1) {
+					this.$set(this, 'mMove', true);
+
+					this.offH += e.movementY;
+					this.offW += e.movementX;
+
+					this.reloadFrame();
+				}
+			},
+
+			reloadFrame: function() {
+				this.loadPic(this.frames[this.idxNow].pic);
+			},
 			nextFrame: function(frame) {
-				this.current = frame.index;
+				this.idxNow = frame.index;
 
 				if(frame.pic) {
 					this.loadPic(frame.pic);
 				}
 
 				this.timeout = setTimeout(frame.loaded, frame.delay);
+			},
+			loadPic: function(pic) {
+				this.picNow = pic;
+
+				let ctx = this.$refs.can.getContext('2d');
+				let box = this.$refs.box;
+
+				let boxW = box.offsetWidth;
+				let boxH = box.offsetHeight;
+
+				let picW = pic.width;
+				let picH = pic.height;
+
+				let finW = picW;
+				let finH = picH;
+
+				ctx.clearRect(this.lastLeft, this.lastTop, this.lastFinW, this.lastFinH);
+
+				ctx.canvas.width = boxW;
+				ctx.canvas.height = boxH;
+
+				if(finW > boxW) {
+					let diff = 1 - ((finW - boxW) / finW);
+
+					finW = ~~(diff*finW);
+					finH = ~~(diff*finH);
+				}
+
+				if(finH > boxH) {
+					let diff = 1 - ((finH - boxH) / finH);
+
+					finW = ~~(diff*finW);
+					finH = ~~(diff*finH);
+				}
+
+				this.lastLeft = (boxW - finW) / 2 + this.offW - finW * ((this.zoom/100 - 1) / 2);
+				this.lastTop = (boxH - finH) / 2 + this.offH - finH * ((this.zoom/100 - 1) / 2);
+				this.lastFinW = finW * this.zoom/100;
+				this.lastFinH = finH * this.zoom/100;
+
+				ctx.drawImage(pic, this.lastLeft, this.lastTop, this.lastFinW, this.lastFinH);
 			},
 			playFrame: function(nowPos = 0) {
 				if(nowPos >= this.frames.length) {
@@ -70,7 +192,7 @@
 					this.nextFrame(now);
 				}
 				else {
-					this.current = nowPos;
+					this.idxNow = nowPos;
 
 					this.loadPic(now.pic);
 
@@ -90,46 +212,12 @@
 					}.bind(this), now.delay);
 				}
 			},
-			loadPic: function(pic) {
-				let ctx = this.$refs.can.getContext('2d');
-				let box = this.$refs.box;
-
-				let boxW = box.offsetWidth;
-				let boxH = box.offsetHeight;
-
-				let picW = pic.width;
-				let picH = pic.height;
-
-				let finW = picW;
-				let finH = picH;
-
-
-				if(finW > boxW) {
-					let diff = 1 - ((finW - boxW) / finW);
-
-					finW = ~~(diff*finW);
-					finH = ~~(diff*finH);
-				}
-
-				if(finH > boxH) {
-					let diff = 1 - ((finH - boxH) / finH);
-
-					finW = ~~(diff*finW);
-					finH = ~~(diff*finH);
-				}
-
-				ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-				this.width = ctx.canvas.width = finW;
-				this.height = ctx.canvas.height = finH;
-
-				ctx.drawImage(pic, 0, 0, finW, finH);
-			},
 			initPlayer: function(illust) {
 				clearInterval(this.timeout);
 				clearInterval(this.interval);
+
 				this.loader = 0;
-				this.current = 0;
+				this.idxNow = 0;
 				let frames = this.frames = illust.frames || illust.stat.frames;
 
 				let iid = illust.iid;
@@ -157,7 +245,6 @@
 
 					if(frames[0] === frame) {
 						pic.addEventListener('load', function() {
-							L('first');
 							this.playFrame();
 						}.bind(this));
 					}
@@ -175,7 +262,13 @@
 	.picCan {
 		position: relative;
 
-		max-width: 100%;
-		max-height: 100%;
+		width: 100%;
+		height: 100%;
+
+		cursor: grab;
+	}
+
+	.mdown {
+		cursor: grabbing;
 	}
 </style>
