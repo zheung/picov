ea81f537b2436d4700a49bb15f354a507ef655f9
DanoR <zheung@vip.qq.com>
2021-12-21 00:47:33 +0800

    cut Fontawesome

--- app/lib/plugin/Fontawesome.js
+++ app/lib/plugin/Fontawesome.js
@@ -1,12 +1,39 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import {
+	faSearch,
+	faAngleLeft,
+	faAngleRight,
+	faAngleDoubleLeft,
+	faAngleDoubleRight,
+	faCompass,
+	faBookOpen,
+	faDownload,
+	faHouseUser,
+	faVideo,
+	faUserEdit,
+	faHome,
+	faPaintBrush,
+} from '@fortawesome/free-solid-svg-icons';
+// import { far } from '@fortawesome/free-regular-svg-icons';
 // import { fab } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export const install = function(app) {
-	library.add(fas);
-	library.add(far);
+	// library.add(fas);
+	// library.add(far);
+	library.add(faSearch);
+	library.add(faAngleLeft);
+	library.add(faAngleRight);
+	library.add(faAngleDoubleLeft);
+	library.add(faAngleDoubleRight);
+	library.add(faCompass);
+	library.add(faBookOpen);
+	library.add(faDownload);
+	library.add(faHouseUser);
+	library.add(faVideo);
+	library.add(faUserEdit);
+	library.add(faHome);
+	library.add(faPaintBrush);
 
 	app.component('Fas', FontAwesomeIcon);
 };
