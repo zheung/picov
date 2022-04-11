e6cd7eecf65a97ce1a7776bcc09200ef2f748356
Nuogz <zheung@vip.qq.com>
2022-04-11 16:03:37 +0800

    v5.15.0 break: new proxy config. Proxy api according host, and `$` as default chore: update packages

--- app/pixiv/get.lib.js
+++ app/pixiv/get.lib.js
@@ -9,9 +9,18 @@ const headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
 };
 
-const proxies = {};
+const proxies = { $: C.proxy?.$ ? new HttpsProxyAgent(C.proxy?.$) : null };
 
-const getProxy = url => url ? (proxies[url] ?? (proxies[url] = new HttpsProxyAgent(url))) : null;
+// const getProxy = url => url ? (proxies[url] ?? (proxies[url] = new HttpsProxyAgent(url))) : null;
+const getProxy = url => {
+	const host = new URL(url).host;
+
+	if(host in proxies) { return proxies[host]; }
+
+	if(host in C.proxy && C.proxy[host]) { return proxies[host] = new HttpsProxyAgent(C.proxy[host]); }
+
+	return proxies.$;
+};
 const getHeader = cookie => Object.assign({ Cookie: `PHPSESSID=${cookie}` }, headers);
 
 
@@ -19,7 +28,7 @@ export const getText = async (url, cookie, params, returnData = true) => {
 	const response = await Axios.get(url, {
 		params,
 		responseType: 'text', headers: getHeader(cookie),
-		httpsAgent: getProxy(C.proxy.api)
+		httpsAgent: getProxy(url)
 	});
 
 	return returnData ? response.data : response;
@@ -29,7 +38,7 @@ export const getText = async (url, cookie, params, returnData = true) => {
 export const head = async (url, cookie) => {
 	return await Axios.head(url, {
 		responseType: 'text', headers: getHeader(cookie),
-		httpsAgent: getProxy(C.proxy.api)
+		httpsAgent: getProxy(url)
 	});
 };
 
@@ -37,7 +46,7 @@ export const getJSON = async (url, cookie, params, returnData = true) => {
 	const response = await Axios.get(url, {
 		params,
 		responseType: 'json', headers: getHeader(cookie),
-		httpsAgent: getProxy(C.proxy.api)
+		httpsAgent: getProxy(url)
 	});
 
 	return returnData ? response.data : response;
@@ -46,7 +55,7 @@ export const getJSON = async (url, cookie, params, returnData = true) => {
 export const postJSON = async (url, cookie, params, headers, returnData = true) => {
 	const response = await Axios.post(url, params, {
 		responseType: 'json', headers: Object.assign(getHeader(cookie), headers),
-		httpsAgent: getProxy(C.proxy.api)
+		httpsAgent: getProxy(url)
 	});
 
 	return returnData ? response.data : response;
@@ -56,7 +65,7 @@ export const postJSON = async (url, cookie, params, headers, returnData = true) 
 export const getStream = async (url, cookie, returnData = true) => {
 	const response = await Axios.get(url, {
 		responseType: 'stream', headers: getHeader(cookie),
-		httpsAgent: getProxy(C.proxy.api)
+		httpsAgent: getProxy(url)
 	});
 
 	return returnData ? response.data : response;
@@ -66,7 +75,7 @@ export const getStream = async (url, cookie, returnData = true) => {
 export const getBuffer = async (url, cookie, returnData = true) => {
 	const response = await Axios.get(url, {
 		responseType: 'arraybuffer', headers: getHeader(cookie),
-		httpsAgent: getProxy(C.proxy.api)
+		httpsAgent: getProxy(url)
 	});
 
 	return returnData ? response.data : response;
