4b2cb7bd9bbe5bf15425b7ef9e769ec2d8c3f1ec
Nuogz <zheung@vip.qq.com>
2023-01-17 17:34:05 +0800

    * update local illust file api

--- src/local/illust/file.api.js
+++ src/local/illust/file.api.js
@@ -7,11 +7,17 @@ import { parse, resolve } from 'path';
 export const method = 'get';
 export const parseResult = false;
 export const parseProfile = true;
-export const handle = ({ file, $profile: profile }, ctx) => {
+export const handle = ({ location, file, $profile: profile }, ctx) => {
 	AS(file, `未指定~[文件]~{${file}}`);
 
+	const dirIllust = location == 'prepare'
+		? profile.dir.illustPrepare
+		: location == 'archive'
+			? profile.dir.illustArchive
+			: undefined;
 
-	const path = resolve(profile.dir.illustPrepare, file);
+
+	const path = resolve(dirIllust, file);
 
 	ctx.type = parse(path).ext;
 	ctx.set('Cache-Control', 'max-age=3600');
